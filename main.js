c3ff28b1dea6ffd19e7d72bb8903067e5f930922
MuhammedMikayiov <mikayilovmuhammed@gmail.com>
2024-12-02 03:46:02 +0400

    Added JS intro code examples

---
+++ main.js
@@ -0,0 +1,125 @@
+// var firstName = "Ibrahim";
+// var lastName = "Mirzayev";
+// var age = 16;
+
+// var isMarried = false;
+
+// var stringType = "";
+
+// var undefinedVariable = undefined;
+
+// var nullVariable = null;
+
+// var objectIbrahim = {
+//   firstName: "Ibrahim",
+//   lastName: "Mirzayev",
+//   age: 16,
+// };
+
+// console.log(objectIbrahim.isStudent);
+
+// objectIbrahim.isStudent = true;
+// objectIbrahim.age = 17;
+
+// var keyName = "lastName";
+// console.log(array[44]);
+
+// console.log(objectIbrahim.isStudent);
+// var array = [1, 2, 3, 4, 5, 6, null, "Salam", true];
+// array[9] = false;
+// array[13] = 22;
+// array[14] = null;
+// console.log(array[9]);
+// console.log(array[10]);
+// console.log(array[11]);
+// console.log(array[12]);
+// console.log(array[13]);
+// console.log(array[14]);
+
+// console.log(objectIbrahim[keyName]);
+// console.log(typeof array);
+
+// console.log(test);
+// var test = "salam";
+// console.log(test);
+// var test = "sagol";
+// console.log(test);
+
+// console.log(test);
+// let test = "Salam";
+// console.log(test);
+
+// let test2 = "sagol";
+// console.log(test2);
+
+// const testConstant = "Hello world";
+
+// const test = ""
+
+// const personObject = {
+//   firstname: "Muhammed",
+//   lastname: "Mikayilov",
+// };
+
+// personObject = "Test";
+
+// personObject.isTeacher = true;
+
+// console.log(personObject);
+
+function sum(a, b) {
+  return a + b;
+}
+
+// console.log(sum(5, 5));
+
+// console.log(sum(10, 20));
+
+// sum(5, 5);
+
+// sum(10, 30);
+
+// function totalSum(a, b) {
+//   return sum(a, b);
+// }
+
+// console.log(totalSum(6, 10));
+// console.log(totalSum(20, 10));
+// console.log(totalSum(6, 50));
+// console.log(totalSum(66, 11));
+
+function test(fn) {
+  return fn();
+}
+
+// const anonymFunction = function () {
+//   return 5 + 10;
+// };
+
+// function exampleFunc() {
+//   return 5 + 10;
+// }
+
+console.log(test(() => 5 + 10));
+
+const tFn = (a, b) => a + b;
+
+console.log("TEST:", tFn(6, 10));
+
+// console.log("TEST:", tFn(5 + 10));
+
+// const anonymFunction = function () {
+//   return 5 + 10;
+// };
+
+// console.log(test(anonymFunction()));
+
+// console.log(test(anonymFunction));
+
+// );
+
+// const testFunc = function () {
+//   return a + b;
+// };
+
+// console.log(a + b);
